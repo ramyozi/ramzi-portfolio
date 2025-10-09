888cd4e8c3655cdb0f20067cb115400aae3c73ba
Ramzi <ramzibenmansour.pro@gmail.com>
2025-10-09 11:50:44 +0200

    feat(queries): include image and file references in GROQ

--- src/sanity/queries/info.ts
+++ src/sanity/queries/info.ts
@@ -1,11 +1,19 @@
 export const aboutMeQuery = `
   *[_type == "aboutMe" && locale == $locale][0]{
-    _id, content, locale
+    _id,
+    content,
+    "cv": cv.asset->{url},
+    "image": image.asset->{url},
+    locale
   }
 `;
 
 export const motivationQuery = `
   *[_type == "motivation" && locale == $locale][0]{
-    _id, content, locale
+    content,
+    locale,
+    "image": image.asset->{
+      url
+    }
   }
 `;
--- src/sanity/queries/projects.ts
+++ src/sanity/queries/projects.ts
@@ -1,29 +1,29 @@
 export const allProjectsQuery = `
-*[_type == "project" && (!defined(locale) || locale == $locale)] | order(dateRange desc) {
-  _id,
-  title,
-  description,
-  status,
-  dateRange,
-  image,
-  gallery,
-  technologies[]{key, label},
-  links{repo, repoFrontend, repoBackend, repoMobile, live},
-  locale
-}
+  *[_type == "project" && (!defined(locale) || locale == $locale)] | order(dateRange desc){
+    _id,
+    title,
+    description,
+    status,
+    dateRange,
+    "image": image.asset->{url},
+    "gallery": gallery[].asset->{url},
+    technologies[]{key,label},
+    links{repo,repoFrontend,repoBackend,repoMobile,live},
+    locale
+  }
 `;
 
 export const projectByIdQuery = `
-*[_type == "project" && _id == $id][0]{
-  _id,
-  title,
-  description,
-  status,
-  dateRange,
-  image,
-  gallery,
-  technologies[]{key, label},
-  links{repo, repoFrontend, repoBackend, repoMobile, live},
-  locale
-}
+  *[_type == "project" && _id == $id][0]{
+    _id,
+    title,
+    description,
+    status,
+    dateRange,
+    "image": image.asset->{url},
+    "gallery": gallery[].asset->{url},
+    technologies[]{key,label},
+    links{repo,repoFrontend,repoBackend,repoMobile,live},
+    locale
+  }
 `;
